c9bbd8ce3a96df4f17c6ca8018b8ec8e264db73c
faezeh-dev <faezeh0078@gmail.com>
2023-07-29 00:08:13 +0330

    change

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,5 @@
 // play pause video
-
+import '../../node_modules/swiper/dist/js/swiper.min.js';
 const playBTN =document.getElementById("PlayPuase")
 const video = document.getElementById("video-elem")
 
--- Js/product.js
+++ Js/product.js
@@ -1,60 +1,26 @@
-// let swiperone = new Swiper(".mySwiper1", {
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-       
-//       },
-//       spaceBetween: 50,
-//       effect: "cube",
-//  grabCursor: true,
-//  cubeEffect: {
-//    shadow: true,
-//    slideShadows: true,
-//    shadowOffset: 20,
-//    shadowScale: 0.94,
-//  },
-//  navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   }
-// });
-// var swipertwo = new Swiper(".Swipertwo", {
-//     slidesPerView: 1,
-//     centeredSlides: false,
-//     slidesPerGroupSkip: 1,
-//     grabCursor: true,
-//     keyboard: {
-//       enabled: true,
-//     },
-//     breakpoints: {
-//       769: {
-//         slidesPerView: 2,
-//         slidesPerGroup: 2,
-//       },
-//     },
-//     scrollbar: {
-//       el: ".swiper-scrollbar",
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
-
-const items = document.querySelectorAll(".menu__item")
-const marker = document.querySelector(".menu__active")
-console.log(items);
-items.forEach((item)=>{
-item.addEventListener('click',(e)=>{
-    indicator(e.target)
-})
-})
-function indicator(item){
-    console.log(item.offsetTop);
-marker.style.top = item.offsetTop;
-
-}
+$(document).ready(function() {
+  $('#responsive').lightSlider({
+      item:4,
+      loop:false,
+      slideMove:2,
+      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+      speed:600,
+      responsive : [
+          {
+              breakpoint:800,
+              settings: {
+                  item:3,
+                  slideMove:1,
+                  slideMargin:6,
+                }
+          },
+          {
+              breakpoint:480,
+              settings: {
+                  item:2,
+                  slideMove:1
+                }
+          }
+      ]
+  });  
+});
