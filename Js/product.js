55529cac347875e297f2ae09b28c2b5e3d5f1876
faezeh-dev <faezeh0078@gmail.com>
2023-07-29 12:30:43 +0330

    slider

--- Js/product.js
+++ Js/product.js
@@ -1,26 +1,45 @@
-$(document).ready(function() {
-  $('#responsive').lightSlider({
-      item:4,
-      loop:false,
-      slideMove:2,
-      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-      speed:600,
-      responsive : [
-          {
-              breakpoint:800,
-              settings: {
-                  item:3,
-                  slideMove:1,
-                  slideMargin:6,
-                }
-          },
-          {
-              breakpoint:480,
-              settings: {
-                  item:2,
-                  slideMove:1
-                }
-          }
-      ]
-  });  
+
+let swiper = new Swiper(".Swiperone", {
+   pagination: {
+      
+       clickable: true,
+      
+     },
+     loop: true,
+     spaceBetween: 50,
+   //   effect: "cube",
+grabCursor: true,
+cubeEffect: {
+  shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.94,
+},
+navigation: {
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+ },
+});
+let swiper1 = new Swiper(".Swipertwo", {
+   pagination: {
+      
+       clickable: true,
+      
+     },
+     slidesPerView: 3,
+     spaceBetween: 30,
+     loop: true,
+  
+   //   effect: "cube",
+grabCursor: true,
+cubeEffect: {
+  shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.94,
+},
+navigation: {
+   nextEl: ".swiper-button-next",
+   prevEl: ".swiper-button-prev",
+ },
 });
