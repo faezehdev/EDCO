d8f759272e0653bb6caa49999eb298f60245761c
faezeh-dev <faezeh0078@gmail.com>
2023-07-29 15:43:57 +0330

    animation menu

--- Js/listProject.js
+++ Js/listProject.js
@@ -1,28 +1,3 @@
-
-let swiper = new Swiper(".Swiperone", {
-    pagination: {
-       
-        clickable: true,
-       
-      },
-      slidesPerView: 5,
-      loop: true,
-      spaceBetween: 10,
-    //   effect: "cube",
- grabCursor: true,
- cubeEffect: {
-   shadow: true,
-   slideShadows: true,
-   shadowOffset: 20,
-   shadowScale: 0.94,
- },
- navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
- });
-
- 
 const btnContainer = document.getElementsByClassName('menu__list');
 let btns = btnContainer[0].getElementsByClassName('menu__link');
 let menuActive = document.getElementsByClassName('menu__active')
--- Js/product.js
+++ Js/product.js
@@ -48,3 +48,17 @@ navigation: {
    prevEl: ".swiper-button-prev",
  },
 });
+
+const btnContainer = document.getElementsByClassName('menu__list');
+let btns = btnContainer[0].getElementsByClassName('menu__link');
+let menuActive = document.getElementsByClassName('menu__active')
+for(let i=0;i<btns.length ; i++){
+  btns[i].addEventListener('click',()=>{
+    let current = document.getElementsByClassName('active')
+    current[0].className = current[0].className.replace(" active", "")
+    btns[i].className += ' active'
+    console.log(current[0].className);
+  })
+ 
+
+  }
